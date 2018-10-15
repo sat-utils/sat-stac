fe40c79941aa7918b7e53eacfb301cf906f97b97
Matthew Hanson <matt.a.hanson@gmail.com>
2018-10-15 13:02:12 -0400

    include STACError in __init__

--- stac/__init__.py
+++ stac/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .thing import Thing
+from .thing import Thing, STACError
 from .catalog import Catalog
 from .collection import Collection
 from .item import Item
