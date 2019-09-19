39ceaf3335756b728af7725f603bc5fdf974c05d
Matthew Hanson <matt.a.hanson@gmail.com>
2019-09-19 16:48:36 -0400

    update tests

--- satstac/__init__.py
+++ satstac/__init__.py
@@ -4,4 +4,4 @@ from .thing import Thing, STACError
 from .catalog import Catalog
 from .collection import Collection
 from .item import Item
-from .items import Items
+from .itemcollection import ItemCollection
